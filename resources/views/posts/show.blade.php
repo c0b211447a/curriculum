6927bf411461424cae4454f3947d2ad1e41b1de2
c0b211447a <c0b211447a@edu.teu.ac.jp>
2023-08-18 18:28:47 +0900

    認証機能の追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,20 +7,25 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:200:600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-        <div class="content">
-            <div class="content_post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>
+    <x-app-layout>
+        <x-slot name="header">
+            Show
+        </x-slot>
+        <body>
+            <h1 class="title">
+                {{ $post->title }}
+            </h1>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+            <div class="content">
+                <div class="content_post">
+                    <h3>本文</h3>
+                    <p>{{ $post->body }}</p>
+                </div>
             </div>
-        </div>
-        <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
-        <div class="back">
-            <a href="/">back</a>
-        </div>
-    </body>
+            <div class="edit"><a href="/posts/{{ $post->id }}/edit">edit</a></div>
+            <div class="back">
+                <a href="/">back</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
